b5d21d722f098c7a5fc28794a87c74528be8dd29
paulo-rssouza <paulopr.dssouza@gmail.com>
2026-08-25 16:38:28 -0300

    Ajusta login do Google para usar o cofre do Streamlit

--- dashboard.py
+++ dashboard.py
@@ -30,7 +30,7 @@ st.markdown("""
 # ==========================================
 @st.cache_resource
 def get_ws():
-    cred = Credentials.from_service_account_file("credenciais.json", scopes=["https://www.googleapis.com/auth/spreadsheets"])
+    cred = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=["https://www.googleapis.com/auth/spreadsheets"])
     client = gspread.authorize(cred)
     return client.open_by_url("https://docs.google.com/spreadsheets/d/1DosfnqIt8ioBxfXMrpEBT8Md7apgsdhvWg0YswUA-IA/edit")
 
